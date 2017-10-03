7fba9f3cdb6a4c41afc839751dab398e1300e8e6
sinkz <diih_augusto@live.com>
2017-10-02 22:50:00 -0300

    Implementação do Redux

---
+++ frontend-redux/src/todo/todoReducer.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = { description: '', list: [] }
+
+ export default (state = INITIAL_STATE, action) => {
+     switch(action.type){
+        case 'DESCRIPTION_CHANGED':
+            return { ...state, description: action.payload }
+        case 'TODO_SEARCH':
+            return { ...state, list: action.payload.data }
+        default:
+            return state
+     }
+ }
